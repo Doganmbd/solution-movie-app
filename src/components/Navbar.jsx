0b58259d2cbee87ad16e63ee7e8a91c1581e3115
mbd <doganmbdogan@gmail.com>
2023-06-17 20:32:25 +0300

    3- CurrenUser oluşturup şarta bağladım

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
+import { Button } from "bootstrap";
 import React from "react";
-import  {Link}  from "react-router-dom";
+import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  /* const currentUser = {displayName : "MBD"} */
+  const currentUser = false;
+
+  
   return (
     <div>
       <nav className="navbar navbar-expand-lg ">
@@ -9,6 +14,21 @@ const Navbar = () => {
           <Link to="/" className="navbar-brand text-white">
             <h4>React Movie App</h4>
           </Link>
+          <div className="d-flex text-white align-items-center">
+            {currentUser ? (
+              <>
+                <h5 className="mb-0 text-capitalize">
+                  {currentUser.displayName}{" "}
+                </h5>
+                <button className="ms-2 btn btn-outline-light">Logout</button>
+              </>
+            ) : (
+              <>
+                <button className="ms-2 btn btn-outline-light">Login</button>
+                <button className="ms-2 btn btn-outline-light">Register</button>
+              </>
+            )}
+          </div>
         </div>
       </nav>
     </div>
