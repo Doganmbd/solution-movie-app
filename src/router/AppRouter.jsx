2154fdc817f45b97ecf8a6e3c8cf35f79b428a9a
mbd <doganmbdogan@gmail.com>
2023-06-17 19:41:47 +0300

    1- Router yapısını oluşturdum

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,5 +1,25 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Navbar from "../components/Navbar";
+import Main from "../pages/Main";
+import Login from "../pages/Login";
+import Register from "../pages/Register";
+import MovieDetail from "../pages/MovieDetail";
+
 const AppRouter = () => {
-  return <div>AppRouter</div>;
+  return (
+    <div>
+      <Navbar />
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/register" element={<Register />} />
+          {/* //! details den sonra ne gelirse gelsisn MovieDetaili render et / absolute ve relative path olarak değişir. */}
+          <Route path="/details/:id" element={<MovieDetail />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default AppRouter;
