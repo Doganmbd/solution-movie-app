9e7b9a61ef101b58c67819e073d5cba71131ef5d
mbd <doganmbdogan@gmail.com>
2023-06-18 15:57:12 +0300

    32- MovieDetail sayfası  PrivateRouter içine aldm

--- src/router/AppRouter.jsx
+++ src/router/AppRouter.jsx
@@ -1,11 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  Outlet,
+  Navigate,
+} from "react-router-dom";
 import Navbar from "../components/Navbar";
 import Main from "../pages/Main";
 import Login from "../pages/Login";
 import Register from "../pages/Register";
 import MovieDetail from "../pages/MovieDetail";
+import { AuthContext } from "../context/AuthContext";
+import { useContext } from "react";
 
 const AppRouter = () => {
+  const { currentUser } = useContext(AuthContext);
+
+  function PrivateRouter() {
+    return currentUser ? <Outlet /> : <Navigate to="/login" replace />;
+  }
+
   return (
     <div>
       <Navbar />
@@ -15,7 +29,9 @@ const AppRouter = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         {/* //! details den sonra ne gelirse gelsisn MovieDetaili render et / absolute ve relative path olarak değişir. */}
-        <Route path="/details/:id" element={<MovieDetail />} />
+        <Route path="/details/:id" element={<PrivateRouter />}>
+          <Route path="" element={<MovieDetail />}/>
+        </Route>
       </Routes>
     </div>
   );
