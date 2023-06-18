531e4cf0446678560483b99e0654b90a9571e4f0
mbd <doganmbdogan@gmail.com>
2023-06-18 17:26:32 +0300

    35- index ve app içine ToastContainer ekledim

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import "bootstrap/dist/css/bootstrap.css";
 import { BrowserRouter } from 'react-router-dom';
+import "react-toastify/dist/ReactToastify.css";
 
 
 
