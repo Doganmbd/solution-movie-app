86e12c1963b9e6c3a3d56f65104143f7061428be
mbd <doganmbdogan@gmail.com>
2023-06-18 08:30:53 +0300

    10- signUp newUser register email,psswrd ekldm

--- src/auth/firebase.jsx
+++ src/auth/firebase.jsx
@@ -1,5 +1,5 @@
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { createUserWithEmailAndPassword, getAuth } from "firebase/auth";
 
 // TODO: Replace the following with your app's Firebase project configuration
 //* https://firebase.google.com/docs/auth/web/start
@@ -19,3 +19,31 @@ const app = initializeApp(firebaseConfig);
 
 // Initialize Firebase Authentication and get a reference to the service
 const auth = getAuth(app);
+
+export const createUser = async (email, password) => {
+  //! yeni bir kullanıcı oluşturmak için kullanılan firebase metodu
+
+  try {
+    let userCredential = await createUserWithEmailAndPassword(
+      auth,
+      email,
+      password
+    );
+    console.log(userCredential);
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+/*createUserWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    // Signed in 
+    const user = userCredential.user;
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    // ..
+  });
+ */
