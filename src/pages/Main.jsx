fd9082c3714ca5cb9a536f388275f37a14e9db81
mbd <doganmbdogan@gmail.com>
2023-06-18 13:42:57 +0300

    21- axios ile data mı çektim

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,16 +1,31 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
+
+const API_KEY = process.env.REACT_APP_TMDB_key;
+
+console.log(API_KEY);
+
+const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
+console.log(FEATURED_API);
+
 
 const Main = () => {
-  const API_KEY = process.env.REACT_APP_TMDB_key;
+  const [movies, setMovies] = useState([]);
 
-  console.log(API_KEY);
+  useEffect(() => {
+    getMovies(FEATURED_API)
+  }, [])
+  
+  const getMovies = (API) => {
+    axios
+      .get(API)
+      .then((res) => console.log(res.data.results))
+      .catch((err) => console.log(err));
+  };
 
-  const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
-  const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
-
-  return <div>
-    
-  </div>;
+  return <div></div>;
 };
 
 export default Main;
