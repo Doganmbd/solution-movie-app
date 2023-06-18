3c563ddd61ac2bdfb15f9d36ef98d087ca8391eb
mbd <doganmbdogan@gmail.com>
2023-06-18 15:36:03 +0300

    30- MovieDetail in temellerini olşturdum

--- src/components/MovieCards.jsx
+++ src/components/MovieCards.jsx
@@ -1,11 +1,12 @@
 import React, { useContext } from "react";
 import { AuthContext } from "../context/AuthContext";
+import { useNavigate } from "react-router-dom";
 
 const IMG_API = "https://image.tmdb.org/t/p/w1280";
 const defaultImage =
   "https://images.unsplash.com/photo-1581905764498-f1b60bae941a?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=700&q=80";
 
-const MovieCards = ({ poster_path, overview, title ,vote_average}) => {
+const MovieCards = ({ poster_path, overview, title ,vote_average,id}) => {
 
 
   const setVoteClass = (vote) => {
@@ -18,15 +19,17 @@ const MovieCards = ({ poster_path, overview, title ,vote_average}) => {
     }
   };
 
+  const navigate = useNavigate()
+
   const {currentUser} = useContext(AuthContext)
   return (
-    <div className="movie">
+    <div className="movie" onClick={()=>navigate("/details/ " + id)}>
       <img
         loading="lazy"
         src={poster_path ? IMG_API + poster_path : defaultImage}
         alt="movie-card"
       />
-      <div className="d-flex align-items-baseline justify-content-between p-1 text-white">
+      <div className="d-flex align-items-baseline justify-content-between p-1 text-white" >
         <h5>{title}</h5>
         {currentUser && (
           <span className={`tag ${setVoteClass(vote_average)}`}>
