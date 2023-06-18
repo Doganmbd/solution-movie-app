6d8f4be93d16e72977a686bee25440ce01e2c89b
mbd <doganmbdogan@gmail.com>
2023-06-18 13:26:31 +0300

    20-TMDB den key aldım env kydm main ekldm

--- src/pages/Main.jsx
+++ src/pages/Main.jsx
@@ -1,9 +1,16 @@
-import React from 'react'
+import React from "react";
 
 const Main = () => {
-  return (
-    <div>Main</div>
-  )
-}
+  const API_KEY = process.env.REACT_APP_TMDB_key;
 
-export default Main
+  console.log(API_KEY);
+
+  const FEATURED_API = `https://api.themoviedb.org/3/discover/movie?api_key=${API_KEY}`;
+  const SEARCH_API = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=`;
+
+  return <div>
+    
+  </div>;
+};
+
+export default Main;
