cbd0bf88dde7eb9eb2be31d3bef7fe45abf5240e
mbd <doganmbdogan@gmail.com>
2023-06-18 12:37:25 +0300

    19- ContinueWithGoogle butonunu çalıştırdım

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { signIn } from "../auth/firebase";
+import { signIn, signUpProvider } from "../auth/firebase";
 import { useNavigate } from "react-router-dom";
 
 const Register = () => {
@@ -13,6 +13,10 @@ const Register = () => {
     console.log(email, password);
     signIn(email,password,navigate)
   };
+
+  const handleProviderLogin = () => {
+    signUpProvider(navigate)
+  }
 
   return (
     <div className="d-flex justify-content-center">
@@ -56,7 +60,7 @@ const Register = () => {
             value="Login"
           />
         </form>
-        <button className="btn btn-primary form-control">
+        <button className="btn btn-primary form-control"onClick={handleProviderLogin}>
           Continue with Google
         </button>
       </div>
